aa67f5836279d1292f6a71734a2bbda2a045a550
selamawitTad <anansh.mami@gmail.com>
2019-06-05 06:33:10 -0700

    add day2 js file

---
+++ Week1/day2.js
@@ -0,0 +1,29 @@
+var weatherTemperature = 40
+console.log(weatherTemperature)
+if ( weatherTemperature < 10 ){
+  console.log("it's cold outside")
+}
+else if ( weatherTemperature > 20 ){
+  console.log("it's warm outside")
+}
+else 
+  console.log("it's nice outside")
+
+
+
+//   function weatherTemperature( temprature ){
+//     if (temprature < 10){
+//   return ("it's cold outside")
+// }
+// else if ( temprature > 20 ){
+//   return ("it's warm outside")
+// }
+// else if ( temprature >=10 && temprature <= 20 ){
+//   return ("it's warm outside")
+// }
+//  else 
+//     return("it's good outside")
+// }
+// console.log (weatherTemperature(12))
+    
+                          
