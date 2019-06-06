8f89f048d7bd566d50ccd4acad50948586e4c0e2
selamawitTad <anansh.mami@gmail.com>
2019-06-06 09:53:32 -0700

    day3 object file

--- Week1/day3.js
+++ Week1/day3.js
@@ -1,21 +1,27 @@
-var book = {
-  title: "bookkk",
- author: "mrX",
-  releaseDate: 1997,
+ var book = {
+ title: "DertoGada",
+ author: "MrX",
+ releaseDate: 1997,
    }
-   console.log(book)  
- console.log(book.title) 
-
-
+ 
+console.log(book)  
+console.log(book.title) 
 
 var food = ["salad", "shiro", "firfir"]
 console.log(food)
- 
-for (i = 0; i<3; i++)
-{
- console.log (food[i])
+var i;
 
+for (i = 0; i<3; i++){
+  console.log (food[i])
  }
-                               
+      var count = 1;
+    while (count <= 10 ){
+      console.log(count);
+          count += 1;
+      
+    }
+
+
+             
                    
 
